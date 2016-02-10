7f95bf64f83de1e56047db4bdf00329525329b1c
Kent Gray <aggie12man@gmail.com>
2016-02-10 01:06:02 -0600

    More updates to Fitness App
    Updates make app more user friendly

--- FitnessApp.cs
+++ FitnessApp.cs
@@ -2,54 +2,69 @@ using System;
 
 namespace Treehouse.FitnessFrog
 {
-  class Program
-  {
-      static void Main()
-      {
-          int runningTotal = 0;
-          bool keepGoing = true;
-        
-          while(keepGoing)
-          {                   
-            // Prompt the user for minutes exercised
-            Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");        
-            string entry = Console.ReadLine();
-            
-            if (entry == "quit")
+    class Program
+    {
+        static void Main()
+        {
+            double runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
             {
-                keepGoing = false;
+                // Prompt the user for minutes exercised
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+                string entry = Console.ReadLine();
+
+                if (entry.ToLower() == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    try
+                    {
+                        double minutes = double.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, hombre!");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go Champ!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You must be Unstoppable!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+                        
+                        // Add minutes exercised to total
+                        runningTotal = runningTotal + minutes;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }
+                  
+
+                    // Display total minutes exercised to the screen                  
+                    Console.WriteLine("You've entered " + runningTotal + " minutes");
+                }
+                // Repeat until the user quits
             }
-            else
-            {            
-              int minutes = int.Parse(entry); 
-              
-              if(minutes <= 10)
-              {
-                  Console.WriteLine("Better than nothing, hombre!");
-              }
-              else if(minutes <= 30)
-              {
-                  Console.WriteLine("Way to go Champ!");
-              }
-              else if(minutes <= 60)
-              {
-                  Console.WriteLine("You must be Unstoppable!");
-              }
-              else
-              {
-                  Console.WriteLine("Okay, now you're just showing off!");
-              }
-                 
-              // Add minutes exercised to total
-              runningTotal = runningTotal + minutes;
-                              
-              // Display total minutes exercised to the screen                  
-              System.Console.WriteLine("You've entered " + runningTotal + " minutes");
-            }
-             // Repeat until the user quits
-          }
-        
-        Console.WriteLine("Goodbye");
-   }
-  }
+
+            Console.WriteLine("Goodbye");
+        }
+    }
 }
