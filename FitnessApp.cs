22e46da9bc889e477921e8a3b3a815a01198de30
Kent Gray <aggie12man@gmail.com>
2016-02-09 23:38:53 -0600

    Fitness App
    C# Fitness Application

---
+++ FitnessApp.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Treehouse.FitnessFrog
+{
+  class Program
+  {
+      static void Main()
+      {
+          int runningTotal = 0;
+          bool keepGoing = true;
+        
+          while(keepGoing)
+          {                   
+            // Prompt the user for minutes exercised
+            Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");        
+            string entry = Console.ReadLine();
+            
+            if (entry == "quit")
+            {
+                keepGoing = false;
+            }
+            else
+            {            
+            int minutes = int.Parse(entry);                   
+               
+            // Add minutes exercised to total
+            runningTotal = runningTotal + minutes;
+                            
+            // Display total minutes exercised to the screen                  
+            System.Console.WriteLine("You've entered " + entry + " minutes");
+            }
+             // Repeat until the user quits
+          }
+        
+        Console.WriteLine("Goodbye");
+   }
+  }
+}
